201396baa3daa927ff02a5e071679e234917a2cc
Happy.L.K <17600221061@qq.com>
2018-12-18 21:00:26 +0800

    spring cloud config

--- config-client/src/main/java/springcloud/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/springcloud/configclient/ConfigClientApplication.java
@@ -1,17 +1,15 @@
-package springcloud.configserver;
+package springcloud.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
 @EnableDiscoveryClient
-public class ConfigServerApplication {
+public class ConfigClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
+        SpringApplication.run(ConfigClientApplication.class, args);
     }
 
 }
--- config-client/src/main/java/springcloud/configclient/controller/TestConfigController.java
+++ config-client/src/main/java/springcloud/configclient/controller/TestConfigController.java
@@ -1,4 +1,24 @@
 package springcloud.configclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @RefreshScope:
+ * 启动类 EurekaProviderApplication 通过 @RefreshScope 开启 SpringCloudConfig 客户端的 refresh 刷新范围，来获取服务端的最新配置，
+ * @RefreshScope要加在声明@Controller声明的类上，否则refresh之后Conroller拿不到最新的值，会默认调用缓存。
+ */
+@RestController
+@RefreshScope
 public class TestConfigController {
+
+    @Value("${form}")
+    private String form;
+
+    @GetMapping("getForm")
+    public String getForm() {
+        return form;
+    }
 }
